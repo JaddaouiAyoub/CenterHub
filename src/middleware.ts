2f03f8efbad0dfea6186586fd36310c2ffe384d3
Ayoub Jaddaoui <a.jaddaoui@deliver4it.com>
2026-04-13 12:09:17 +0100

    test middlewaresize

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,34 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
-import { NextRequest, NextResponse } from "next/server";
+import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
+import { auth } from "@/auth"
 
 const i18nMiddleware = createMiddleware(routing);
 
-export default function middleware(req: NextRequest) {
-  const isAuthPage = req.nextUrl.pathname.includes("/login");
 
-  const session =
-    req.cookies.get("next-auth.session-token") ||
-    req.cookies.get("__Secure-next-auth.session-token");
+export default auth((req) => {
+  const isAuthPage = req.nextUrl.pathname.includes('/login');
+  const session = req.auth;
 
-  const isLoggedIn = !!session;
-
-  if (isAuthPage && isLoggedIn) {
-    return NextResponse.redirect(
-      new URL(`/${req.nextUrl.locale || "fr"}/dashboard`, req.url)
-    );
+  if (isAuthPage) {
+    if (session) {
+      return Response.redirect(new URL(`/${req.nextUrl.locale || 'fr'}/dashboard`, req.nextUrl));
+    }
+    return i18nMiddleware(req);
   }
 
-  if (!isLoggedIn && req.nextUrl.pathname.includes("/dashboard")) {
-    return NextResponse.redirect(
-      new URL(`/${req.nextUrl.locale || "fr"}/login`, req.url)
-    );
+  if (!session && req.nextUrl.pathname.includes('/dashboard')) {
+    return Response.redirect(new URL(`/${req.nextUrl.locale || 'fr'}/login`, req.nextUrl));
   }
 
   return i18nMiddleware(req);
-}
+})
 
 export const config = {
-  matcher: ["/((?!api|_next|static|_vercel|.*\\..*).*)"],
+  // Match all pathnames except for
+  // - /api (API routes)
+  // - /_next (Next.js internals)
+  // - /static (static files)
+  // - /_vercel (Vercel internals)
+  // - favicon.ico, sitemap.xml, robots.txt (static files)
+  matcher: ['/((?!api|_next|static|_vercel|[\\w-]+\\.\\w+).*)']
 };
